d689674b416600873dc317ba1be5b264ee365d63
AkramovFozil <fozil.9909@gmail.com>
2024-12-12 11:11:58 +0500

    First Commit

---
+++ AutoTech/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from AutoTech.models import Vehicles, Sensors, ServiceCenters, Maintenance
+
+
+class VehiclesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vehicles
+        fields = ['id', 'model', 'year', 'vin']
+
+
+class SensorsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sensors
+        fields = ['id', 'type', 'installed_date', 'vehicle']
+
+
+class MaintenanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Maintenance
+        fields = ['id', 'service_type', 'service_date', 'vehicle']
+
+
+class ServiceCentersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ServiceCenters
+        fields = ['id', 'name', 'address', 'rating']
